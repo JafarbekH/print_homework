2e702dd4b8d49e0499ed692f6fe01c6932a296df
JafarbekH <zokirovjafar2004@gmail.com>
2022-12-15 20:38:04 +0500

    homework yechildi

---
+++ arithmetic_op01.py
@@ -0,0 +1,7 @@
+# Create a variable 'num_one'  and assign it the value of the int 5.
+x=5
+# Create a variable 'num_two'   and assign it the value of the int 4.
+y=4
+# Find the sum of  'num_one' and 'num_two' then assign to 'variable_total'
+sum=x+y
+print("ikki sonning yig'indisi=",sum)
